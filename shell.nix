84d5c5fabaed84148b377665999e874c3ff7b2fe
amatgil <amatgilvinyes@gmail.com>
2024-05-30 18:46:29 +0200

    Initting

---
+++ shell.nix
@@ -0,0 +1,29 @@
+{
+  pkgs ? import <nixpkgs> { },
+  lib,
+}:
+let
+  packages = with pkgs; [
+    rust-analyzer
+    rustfmt
+    clippy
+    mold
+    trunk
+    llvmPackages.bintools
+
+    #wayland
+    #xorg.libX11
+    #xorg.libXcursor
+  ];
+in
+pkgs.mkShell {
+  # Get dependencies from the main package
+  inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
+  nativeBuildInputs = packages;
+  buildInputs = packages;
+  env = {
+    LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+    LD_LIBRARY_PATH = "${lib.makeLibraryPath packages}";
+    CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "lld";
+  };
+}
